372fbd7009bc029f83c216580a6e50b2fa23cc62
nico <nicolas_rodriguez_94@hotmail.com>
2022-04-26 16:44:35 -0300

    primer commit

---
+++ src/app/pais/pages/ver-pais/ver-pais.component.ts
@@ -0,0 +1,38 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { switchMap, tap } from 'rxjs/operators';
+import { Country } from '../../interfaces/pais.interface';
+import { PaisService } from '../../services/pais.service';
+
+@Component({
+  selector: 'app-ver-pais',
+  templateUrl: './ver-pais.component.html',
+  styles: [
+  ]
+})
+export class VerPaisComponent implements OnInit {
+
+  pais!: Country;
+  constructor(
+    private activateRoute: ActivatedRoute,
+    private paisService: PaisService
+  ) { }
+
+  ngOnInit(): void {
+    // this.activateRoute.params
+    //   .subscribe(({ id }) => {
+    //     this.paisService.buscarPaisPorId(id).subscribe(pais => {
+
+    //     })
+
+    //})
+    this.activateRoute.params
+      .pipe(
+        switchMap(({ id }) => this.paisService.buscarPaisPorId(id)),
+        tap(console.log)
+      )
+      .subscribe(pais => this.pais = pais)
+
+  }
+
+}
